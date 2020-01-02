449a4552b12e9df9dbc7fd54a57af2c49ba0a6ce
Encho Belezirev <encho.belezirev@sap.com>
2020-01-02 22:19:26 +0200

    Add docker image build for the monitoring

--- setup.sh
+++ setup.sh
@@ -27,6 +27,11 @@ fi
 
 cd ${working_dir}/iot
 echo "Setting up server..."
+device_monitoring_image_exists=$(docker images device-monitoring | grep device-monitoring)
+if [[ ${device_monitoring_image_exists} != "" ]]; then
+    echo "Preparing image for the device monitoring..."
+    docker build -t device-monitoring .
+fi
 kafka_started=$(docker ps -a | grep kafka);
 if [[ ${kafka_started} != "" ]]; then
     echo "Docker swarm is running - restarting server..."
