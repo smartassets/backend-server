cce30c13e732f2f15c3f0208f98aa42bb14fe29d
Encho Belezirev <encho.belezirev@sap.com>
2020-03-15 17:31:53 +0200

    Remove unneeded configuration

--- backend/controller/src/main/java/org/industrial/iot/monitoring/discovery/ServiceDiscovery.java
+++
@@ -1,9 +0,0 @@
-package org.industrial.iot.monitoring.discovery;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class ServiceDiscovery {
-    
-    // On regular basis, ping the devices in order to get their health
-}
